941724551616ecd33ae9d7312b79ee8f2ad61652
John Goossen <110130147+John-Goossen@users.noreply.github.com>
2025-03-12 11:50:23 -0500

    Updated Get-EXOMailboxinfo

--- Get-EXOMailboxInfo.ps1
+++ Get-EXOMailboxInfo.ps1
@@ -7,18 +7,19 @@ $i = 1
 $EXOMailboxes | ForEach-Object {
     $i++
     $mbx = $_
-    $mbs = Get-EXOMailboxStatistics -Identity $_.UserPrincipalName -Properties TotalDeletedItemSize, TotalItemSize
+    $mbs = Get-EXOMailboxStatistics -Identity $_.UserPrincipalName -Properties LastLogonTime, TotalDeletedItemSize, TotalItemSize
     Write-Progress -Activity "Processing $mbx" -Status "$i of $EXOMailboxCount completed"
     $Results += New-Object PSObject -Property @{
         UserPrincipalName = $mbx.UserPrincipalName
-        DisplayName = $mbx.DisplayName
+#        DisplayName = $mbx.DisplayName
+        LastLogonTime = $mbs.LastLogonTime
         RecipientTypeDetails = $mbx.RecipientTypeDetails
-        RetentionPolicy = $mbx.RetentionPolicy
-        IssueWarningQuota = $mbx.IssueWarningQuota
-        ProhibitSendQuota = $mbx.ProhibitSendQuota
-        ProhibitSendReceiveQuota = $mbx.ProhibitSendReceiveQuota
-        UseDatabaseQuotaDefaults = $mbx.UseDatabaseQuotaDefaults
-        TotalDeletedItemSize = $mbs.TotalDeletedItemSize
+#        RetentionPolicy = $mbx.RetentionPolicy
+#        IssueWarningQuota = $mbx.IssueWarningQuota
+#        ProhibitSendQuota = $mbx.ProhibitSendQuota
+#        ProhibitSendReceiveQuota = $mbx.ProhibitSendReceiveQuota
+#        UseDatabaseQuotaDefaults = $mbx.UseDatabaseQuotaDefaults
+#        TotalDeletedItemSize = $mbs.TotalDeletedItemSize
         TotalItemSize = $mbs.TotalItemSize
     }
     $Results | Export-Csv "exomailboxinfo.csv" -NoTypeInformation
